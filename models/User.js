4c6fb913e5f6e7c0b62cb61b43785ef76e1a7a84
pdguerra12 <paul.guerra09@gmail.com>
2022-02-02 11:08:23 -0600

    new files, models, routes, and server udpate

---
+++ models/User.js
@@ -0,0 +1,69 @@
+const bcrypt = require("bcrypt");
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+// create our User model
+class User extends Model {
+	checkPassword(pwlogin) {
+		return bcrypt.compareSync(pwlogin, this.password);
+	}
+}
+
+// define table columns and configuration
+User.init(
+	{
+		// define an id column
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		// define a username column
+		username: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		// define an email column
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
+			validate: {
+				isEmail: true,
+			},
+		},
+		// define a password column
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: [4],
+			},
+		},
+	},
+	{
+		hooks: {
+			async beforeCreate(newUserData) {
+				// set up beforeCreate lifecycle "hook" functionality
+				newUserData.password = await bcrypt.hash(newUserData.password, 10);
+				return newUserData;
+			},
+
+			async beforeUpdate(updatedUserData) {
+				updatedUserData.password = await bcrypt.hash(
+					updatedUserData.password,
+					10
+				);
+				return updatedUserData;
+			},
+		},
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: "user",
+	}
+);
+
+module.exports = User;
